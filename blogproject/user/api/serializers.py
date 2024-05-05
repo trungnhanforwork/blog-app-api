f47cabfd492ca0d77ffbf111a234992c32195dce
manle423 <trungmanle.vm@gmail.com>
2024-05-05 15:19:33 +0700

    change password2 to retype_password

--- blogproject/user/api/serializers.py
+++ blogproject/user/api/serializers.py
@@ -3,19 +3,19 @@ from rest_framework import serializers
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    retype_password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['username', 'email', 'password', 'retype_password']
         extra_kwargs = {'password' : {'write_only': True}}
         
     def save(self):
 
         password = self.validated_data['password']
-        password2 = self.validated_data['password2']
+        retype_password = self.validated_data['retype_password']
 
-        if password != password2:
+        if password != retype_password:
             raise serializers.ValidationError({'error': 'Password is not match!'})
 
         if User.objects.filter(email=self.validated_data['email']).exists():
