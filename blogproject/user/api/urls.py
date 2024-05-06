4d458bbc8c59c5c25d075c87e90894949f9eed0e
manle423 <trungmanle.vm@gmail.com>
2024-05-06 16:10:40 +0700

    add Update and Change pass for User

--- blogproject/user/api/urls.py
+++ blogproject/user/api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user.api.views import logout_view, registration_view
+from user.api.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("register/", registration_view, name="register"),
     path("login/", obtain_auth_token, name="login"),
     path("logout/", logout_view, name="logout"),
+    path("profile/", profile_view, name="profile"),
+    path("update/", UpdateProfileView.as_view(), name="update_user"),
+    path("changepassword/", ChangePasswordView.as_view(), name="change_password"),
+    
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
